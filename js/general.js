4dbdfe2e658a1aa0224ab3d587c492b12aa527f1
Karen <karen.azoulay@edu.devinci.fr>
2021-10-13 16:52:02 +0200

    ADD page jeux

--- js/general.js
+++ js/general.js
@@ -2,6 +2,17 @@ $('#btn').click(function () {
     $('#e1').css('display', 'block');
 })
 
+//Test d'entrée dans le jeu
+const codeSecret = 830;
+function codeEntrer() {
+    let code = document.getElementById("code").value;
+    if (code === codeSecret) {
+        window.location.replace("https://jqntn.github.io/CyberGame/etape1.html");
+    }else{
+        document.getElementById("msgCodeError").innerHTML = error;  
+    }
+}
+
 //Formulaire connexion
 const pseudoAdmin = "karen";
 const mdpAdmin = "123";
@@ -10,8 +21,6 @@ var error = "Les informations sont erronés !";
 function validateForm() {
     let pseudo = document.getElementById("pseudo").value;
     let mdp = document.getElementById("mdp").value;
-    console.log(pseudo);
-    console.log(mdp);
     if (pseudo === pseudoAdmin && mdp === mdpAdmin) {
         window.location.replace("https://jqntn.github.io/CyberGame/panelAdmin.html");
     }else{
